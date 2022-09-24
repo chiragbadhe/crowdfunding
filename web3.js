dd33c815619b75840ee25409d84102966ef4cac2
chiragbadhe <badhechirag633@gmail.com>
2022-09-24 14:19:39 +0530

    Ft: add readme

--- web3.js
+++ web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // we are on the server *OR* meta mask is not running
   // creating our own provider
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/08ac79d88b5d4aea961ca36af7ea6ee7"
+    "YOUR_INFURA_KEY"
   );
 
   web3 = new Web3(provider);
